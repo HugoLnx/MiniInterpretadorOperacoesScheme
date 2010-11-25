d39853d3c4103c423209361c336be8cf7e50646f
Roque <roque@ubuntu.ubuntu-domain>
2010-11-25 17:15:23 -0600

    corrigindo identacao

--- lib/lista.rb
+++ lib/lista.rb
@@ -6,7 +6,7 @@ class Lista
 		lista_str.gsub!(/\((.*)\)/){$1}
 		lista_str.gsub!(/(\([0-9 \+\-\/\*]+\))/){Lista.new($1).resolver}
     elementos =  lista_str.split ' '
-		elementos.delete_if{|elemento| elemento.empty?}
+	  elementos.delete_if{|elemento| elemento.empty?}
 		@sinal = elementos.delete_at 0
 		@numeros = elementos.collect{|elemento| elemento.to_number}
 	end
