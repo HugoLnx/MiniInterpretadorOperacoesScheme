178affc9d085acf6a41025433e4121f28afc08ae
Roque <roque@ubuntu.ubuntu-domain>
2010-11-25 17:18:57 -0600

    tentando corrigir identacao de novo xD

--- lib/lista.rb
+++ lib/lista.rb
@@ -3,12 +3,12 @@ class Lista
 	attr_reader :sinal
 
   def initialize(lista_str)
-		lista_str.gsub!(/\((.*)\)/){$1}
-		lista_str.gsub!(/(\([0-9 \+\-\/\*]+\))/){Lista.new($1).resolver}
+    lista_str.gsub!(/\((.*)\)/){$1}
+    lista_str.gsub!(/(\([0-9 \+\-\/\*]+\))/){Lista.new($1).resolver}
     elementos =  lista_str.split ' '
-	  elementos.delete_if{|elemento| elemento.empty?}
-		@sinal = elementos.delete_at 0
-		@numeros = elementos.collect{|elemento| elemento.to_number}
+    elementos.delete_if{|elemento| elemento.empty?}
+    @sinal = elementos.delete_at 0
+    numeros = elementos.collect{|elemento| elemento.to_number}
 	end
 	
 	def resolver
